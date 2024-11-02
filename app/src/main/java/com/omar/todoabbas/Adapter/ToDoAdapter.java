e51ef769499de3c918cd3a04f7e02c8db0b91e1e
Omar Abdelrahman Abbas <benjami4n@gmail.com>
2024-11-03 00:53:04 +0200

    ➕ new: implement ToDoAdapter for managing tasks in RecyclerView
    - Create ToDoAdapter to bind ToDoModel data to the RecyclerView.
    - Define ViewHolder pattern for efficient view management.
    - Implement methods for item creation, binding, and updating task list.

---
+++ app/src/main/java/com/omar/todoabbas/Adapter/ToDoAdapter.java
@@ -0,0 +1,57 @@
+package com.omar.todoabbas.Adapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CheckBox;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.omar.todoabbas.MainActivity;
+import com.omar.todoabbas.Model.ToDoModel;
+import com.omar.todoabbas.R;
+
+import java.util.List;
+
+public class ToDoAdapter extends RecyclerView.Adapter<ToDoAdapter.ViewHolder> {
+
+    private List<ToDoModel> todoList;
+    private MainActivity activity;
+
+    public ToDoAdapter(MainActivity activity) {
+        this.activity = activity;
+    }
+
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.task_layout, parent, false);
+        return new ViewHolder(itemView) ;
+    }
+
+
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        ToDoModel item = todoList.get(position);
+        holder.task.setText(item.getTask());
+        holder.task.setChecked(item.getStatus() == 1);
+    }
+
+    public int getItemCount(){
+        return todoList.size();
+    }
+
+
+    public void  setTasks(List<ToDoModel> todoList) {
+        this.todoList = todoList;
+        notifyDataSetChanged();
+    }
+
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        CheckBox task;
+        ViewHolder(View view) {
+            super(view);
+            task = view.findViewById(R.id.todoCheckBox);
+        }
+    }
+
+}
