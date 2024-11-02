a57c891ee7261858d4cac9b4faabf7d60bde31b7
Omar Abdelrahman Abbas <benjami4n@gmail.com>
2024-11-03 00:53:51 +0200

    🚀 feat: implement MainActivity to manage tasks
    - Set up RecyclerView for displaying tasks.
    - Initialize ToDoAdapter and bind it to the RecyclerView.
    - Create a sample task list with placeholder tasks for demonstration.
    - Enable edge-to-edge display for a modern UI.

--- app/src/main/java/com/omar/todoabbas/MainActivity.java
+++ app/src/main/java/com/omar/todoabbas/MainActivity.java
@@ -7,18 +7,41 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.omar.todoabbas.Adapter.ToDoAdapter;
+import com.omar.todoabbas.Model.ToDoModel;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    private RecyclerView tasksRecyclerView;
+    private ToDoAdapter tasksAdapter;
+
+    private List<ToDoModel> taskList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        if(getSupportActionBar() != null)
+            getSupportActionBar().hide();
+
+        taskList = new ArrayList<>();
+
+        tasksRecyclerView = findViewById(R.id.tasksRecyclerView);
+        tasksRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        tasksAdapter = new ToDoAdapter(this);
+        tasksRecyclerView.setAdapter(tasksAdapter);
+
+        taskList.add(new ToDoModel(1, 0, "Task 1: Omar Should Implement This Task"));
+        taskList.add(new ToDoModel(1, 0, "Task 2: Omar Should Implement This Task"));
+
+        tasksAdapter.setTasks(taskList);
     }
 }
