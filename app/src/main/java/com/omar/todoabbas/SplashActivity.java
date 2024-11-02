29cb8b721a6f3ee946959e4d5a084d4d119b0427
Omar Abdelrahman Abbas <benjami4n@gmail.com>
2024-11-03 00:52:15 +0200

    ➕ new: implement SplashActivity for splash screen functionality
    - Set up SplashActivity with edge-to-edge display and hidden action bar.
    - Add delay to transition from SplashActivity to MainActivity after 2 seconds.
    - Include commented code for handling window insets (future enhancement).

--- app/src/main/java/com/omar/todoabbas/SplashActivity.java
+++ app/src/main/java/com/omar/todoabbas/SplashActivity.java
@@ -1,6 +1,8 @@
 package com.omar.todoabbas;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +17,23 @@ public class SplashActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_splash);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        if(getSupportActionBar() != null)
+            getSupportActionBar().hide();
+
+        final Intent i = new Intent(SplashActivity.this, MainActivity.class);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(i);
+                finish();
+            }
+        }, 2000);
+
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+//            return insets;
+//        });
     }
 }
